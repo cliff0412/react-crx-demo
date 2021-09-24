f94ec7ed0a151c1a7be51e46da744715806f25aa
weitao.yang@bybit.com <sg53311ml@SG53311MLs-MacBook-Pro.local>
2021-09-24 23:25:44 +0800

    add content

--- src/content/index.js
+++ src/content/index.js
@@ -1,1 +1,16 @@
-console.log("content")
+import React from "react";
+import ReactDOM from "react-dom";
+import "./content.styl";
+
+function Content() {
+  return (
+    <div className="CRX-content">
+      <div className="content-entry"></div>
+    </div>
+  );
+}
+
+const app = document.createElement('div')
+app.id = 'CRX-container'
+document.body.appendChild(app)
+ReactDOM.render(<Content />, app)
