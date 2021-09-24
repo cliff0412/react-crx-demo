f75a695e09b5148c0c7d0b0337542af7d877bf1e
weitao.yang@bybit.com <sg53311ml@SG53311MLs-MacBook-Pro.local>
2021-09-24 23:06:51 +0800

    add background

--- src/background/index.js
+++ src/background/index.js
@@ -1,1 +1,19 @@
-console.log("background")
+// 一定要加上下面的global chrome, 要不编译会报错 chrome is not defined
+/*global chrome*/
+chrome.runtime.onInstalled.addListener(function(details) {
+    // console.log(details)
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            // 运行插件的页面url规则
+            conditions: [
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: {}
+                })
+
+            ],
+            actions: [
+                new window.chrome.declarativeContent.ShowPageAction()
+            ]
+        }])
+    })
+})
